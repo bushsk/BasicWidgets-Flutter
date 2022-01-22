9a186f3cb2dfd9c83038b036abf953d74cede7f0
bushra <bushra.sk@siesgst.ac.in>
2022-01-22 12:24:35 +0530

    added icons action

--- stateless_widgets/lib/main.dart
+++ stateless_widgets/lib/main.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
+  
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String msg = 'Click on Icons to see the msg';
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner : false,
         title: "My stateless widget",
         theme: ThemeData(primarySwatch: Colors.green),
         home: Scaffold(
             appBar: AppBar(
-              title: new Text("mera appbar"),
+              title: new Text("my appbar"),
+              leading: new Icon(Icons.menu),
+              actions: <Widget>[
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        msg = 'You pressed arrow icon';
+                      });
+                    },
+                    icon: const Icon(Icons.forward)),
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        msg = 'You pressed close icon';
+                      });
+                    },
+                    icon: const Icon(Icons.close)),
+              ],
+
+              backgroundColor: Colors.pink,
+              elevation: 30.0,
+              //centerTitle: true,
+              titleSpacing: 70.0,
+              //toolbarOpacity: 0.5,
             ),
             body: Container(
               //alignment: Alignment.bottomLeft,
@@ -27,9 +59,70 @@ class MyApp extends StatelessWidget {
                 border: Border.all(color: Colors.black, width: 4),
                 borderRadius: BorderRadius.circular(8),
                 boxShadow: [
-                  new BoxShadow(
+                  BoxShadow(
                     color: Colors.green,
-                    offset: new Offset(6.0, 6.0),
+                    offset: const Offset(6.0, 6.0),
+                  ),
+                ],
+              ),
+
+              child:  Text(msg,
+                  style: TextStyle(
+                    fontFamily: 'sans serif',
+                    color: Colors.yellow,
+                    fontStyle: FontStyle.italic,
+                    shadows: [
+                      Shadow(
+                          color: Colors.blue,
+                          offset: Offset(1, 1),
+                          blurRadius: 2)
+                    ],
+                  )
+                  ),
+            )
+            )
+            
+    );
+  }
+}
+
+/* class MyApp extends State<MyApp> {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "My stateless widget",
+        theme: ThemeData(primarySwatch: Colors.green),
+        home: Scaffold(
+            appBar: AppBar(
+              title: new Text("my appbar"),
+              leading: new Icon(Icons.menu),
+              actions: <Widget>[
+                new IconButton(onPressed: (){print('You pressed arrow icon');}, icon: new Icon(Icons.forward)),
+                new IconButton(onPressed: (){print('You pressed close icon');}, icon: new Icon(Icons.close)),
+              ],
+
+              backgroundColor: Colors.pink,
+              elevation: 30.0,
+              //centerTitle: true,
+              titleSpacing: 70.0,
+              //toolbarOpacity: 0.5,
+            ),
+            body: Container(
+              //alignment: Alignment.bottomLeft,
+              // width: 200.0,
+              // height: 100.0,
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(35),
+              //color: Colors.yellow,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 4),
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                   BoxShadow(
+                    color: Colors.green,
+                    offset:  const Offset(6.0, 6.0),
                   ),
                 ],
               ),
@@ -51,3 +144,4 @@ class MyApp extends StatelessWidget {
             )));
   }
 }
+ */
